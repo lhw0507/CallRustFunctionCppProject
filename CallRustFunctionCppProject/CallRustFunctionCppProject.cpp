f399686a5dc497fe97c62ce51799706df584cd7a
lhw0507 <lhw0507>
2022-06-23 17:53:09 +0900

    rust 구조체 반환 테스트 cpp project

---
+++ CallRustFunctionCppProject/CallRustFunctionCppProject.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+struct RustObject
+{
+	int Key;
+	double dDiameter;
+	int enType;
+};
+
+extern "C" void hello();
+extern "C" int int_from_rust();
+extern "C" RustObject struct_from_rust();
+
+int main()
+{
+	hello();
+
+	auto object = struct_from_rust();
+
+	std::cout << int_from_rust() << std::endl;
+
+	std::cout << object.Key << std::endl;
+	std::cout << object.dDiameter << std::endl;
+	std::cout << object.enType << std::endl;
+
+	int a = 10;
+}
